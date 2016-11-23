246814e19edabd0a743f3384d0d3182f8315bc2d
Dan <dan1ate2@gmail.com>
2016-11-23 13:55:56 +1000

    Advanced custom fields

---
+++ wp-content/themes/dan-hogan/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Dan_Hogan
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dan-hogan' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'dan-hogan' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'dan-hogan' ), 'dan-hogan', '<a href="http://underscores.me/" rel="designer">Dan Hogan</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
